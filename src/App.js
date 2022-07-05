63cf5c4dca407e677e8ec3312601c0d0eeb43b1b
Mike Janes <mjanes@veritone.com>
2022-07-05 10:42:20 -0400

    test deploy

--- src/App.js
+++ src/App.js
@@ -1,7 +1,26 @@
 import logo from './logo.svg';
 import './App.css';
+import { initializeApp } from "firebase/app";
 
 function App() {
+
+  // Import the functions you need from the SDKs you need
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyB0cIUlBq6133vWIhM0ccav8i-Yh_2ncCQ",
+  authDomain: "testdeploy-5fd1b.firebaseapp.com",
+  projectId: "testdeploy-5fd1b",
+  storageBucket: "testdeploy-5fd1b.appspot.com",
+  messagingSenderId: "178875935475",
+  appId: "1:178875935475:web:a96c4f7176ec5af5126586"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
   return (
     <div className="App">
       <header className="App-header">
